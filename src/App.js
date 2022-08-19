252ee270ab4ae2a1db6360187e84765bf88c0f37
Justin <justinrkinsman@gmail.com>
2022-08-19 10:00:55 -0300

    Add functions to app.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,30 @@ export class App extends Component {
     };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    })
+  }
+
+  onSubmitTask = (e) => {
+    e.preventDefault()
+    this.setState({
+      task: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    })
+  }
+
   render() {
     const { task, tasks } = this.state
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter tasks</label>
-          <input type='text' id='taskInput'/>
+          <input onChange={this.handleChange} value={task.text} type='text' id='taskInput'/>
           <button type='submit'>Add Task</button>
         </form>
       </div>
